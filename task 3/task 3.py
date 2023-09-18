134f30ac13085f7ae8f2e63716213fe8a1ed8954
yamen <yamenmuammar1999@gmail.com>
2023-09-18 14:47:35 +0300

    task 3

--- task 3/task 3.py
+++ task 3/task 3.py
@@ -1,25 +1,28 @@
+print("ADD")
 def add(x=0,y=0):
     total = x + y
     print(total)
 num1 = int(input("Enter Number 1 :"))
 num2 = int(input("Enter Number 2 :"))
 add(num1,num2)
-
+print("SUB")
 def sub(x=0,y=0):
     total = x - y
     print(total)
 num1 = int(input("Enter Number 1 :"))
 num2 = int(input("Enter Number 2 :"))
 sub(num1,num2)
+print("DIV")
 def div(x=0,y=0):
     total = x / y
     print(total)
 num1 = int(input("Enter Number 1 :"))
 num2 = int(input("Enter Number 2 :"))
 div(num1,num2)
-def sub(x=0,y=0):
+print("MUL")
+def mul(x=0,y=0):
     total = x * y
     print(total)
 num1 = int(input("Enter Number 1 :"))
 num2 = int(input("Enter Number 2 :"))
-sub(num1,num2)
+mul(num1,num2)
