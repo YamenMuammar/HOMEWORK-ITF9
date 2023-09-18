5b1bc5f063c0fb32b6f90f4de8cab085b959f78d
yamen <yamenmuammar1999@gmail.com>
2023-09-18 12:32:19 +0300

    task 3

---
+++ task 3/task 1.py
@@ -0,0 +1,6 @@
+x = 30
+y = 40
+print(x + y)
+print(x - y)
+print(y - x)
+print(x * y)
--- test.py
+++ test.py
@@ -1,5 +1,28 @@
-print("Yamen Muammar")
-num_1 = input("enter num 1")
-num_2 = input("enter num 2")
+#firs_name = input("inter your first Name :")
+#last_name = input("enter your last name :")
+#age = input("enter your age :")
+#form = f"""
+#>> first Name :{firs_name}
+#>> last Name : {last_name}
+#>> age : {age}"""
+#print(form)
 
-print(num_1,num_2)
+#my_str = "Welcome GSG Itf 9 2023"
+#print(my_str[0:9])
+#print(len(my_str))
+#prr = "    yamenmua@gimal.com    "
+#print(prr.strip())
+#prr = prr.strip()
+#print(prr.replace(prr[1:4],"#########"))
+#my_name = "Yamen Muammar"
+#print(my_name.split(" "))
+
+#x = 5
+#x += 5   # x>(5) + 5 = 10
+#print(x)
+#x **= 3
+#print(x)
+
+#x = 10
+#y =5
+#print(x <= y)
