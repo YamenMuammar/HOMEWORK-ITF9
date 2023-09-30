5d0103f312f86894ee2d6ab93b39b95148618b40
Yamen Muammar <144543346+YamenMuammar@users.noreply.github.com>
2023-09-30 12:23:16 +0300

    Update task 5 (Q2).py

--- task 5/task 5 (Q2).py
+++ task 5/task 5 (Q2).py
@@ -7,7 +7,7 @@ def Area_rectangle(length,width):
 def perimeter_rectangle(length,width):
     if length <= 0 or width <= 0:
         raise ValueError("length and width are not positive numbers")
-    perimeter = (width + length) / 2
+    perimeter = (width + length) * 2
     return perimeter
 
 try:
